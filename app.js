c2001162660853ea0e91e87b2bc0717f43f4b4ab
roquib <roquib01@outlook.com>
2018-10-25 20:49:12 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("click",function() {
+    document.body.style.backgroundColor="#222";
+});
+document.addEventListener("dblclick",function() {
+    document.body.style.backgroundColor="#999";
+});
+document.getElementById("demo").innerHTML="Hei,I am Abdur roquib!";
